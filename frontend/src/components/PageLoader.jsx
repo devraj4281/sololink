a9b95646ceec0677031ad27f0e3210b123b802c2
devraj42817 <rajdev20774@gmail.com>
2025-11-14 17:57:06 +0530

    updated

---
+++ frontend/src/components/PageLoader.jsx
@@ -0,0 +1,13 @@
+
+import { Loader, LoaderIcon } from 'lucide-react'
+import React from 'react'
+
+function PageLoader() {
+  return (
+    <div className='flex items-center justify-center h-screen'>
+      <LoaderIcon className='size-10 animate-spin'/>
+    </div>
+  )
+}
+
+export default PageLoader
---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,21 @@
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios";
+
+export const useAuthStore = create((set, get) => ({
+  authUser: null,
+  isCheckingAuth: true,
+
+  checkAuth: async () => {
+    try {
+      const res = await axiosInstance.get("/auth/check");
+      set({ authUser: res.data });
+      get().connectSocket();
+    } catch (error) {
+      console.log("Error in authCheck:", error);
+      set({ authUser: null });
+    } finally {
+      set({ isCheckingAuth: false });
+    }
+  },
+
+}));
