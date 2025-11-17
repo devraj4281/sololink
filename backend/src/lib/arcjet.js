a083fd82a083b13872a1126c3799e4811d29611c
devraj42817 <rajdev20774@gmail.com>
2025-11-17 23:11:53 +0530

    update arcjet ,db,socket, and utilis libs

--- backend/src/lib/arcjet.js
+++ backend/src/lib/arcjet.js
@@ -11,11 +11,7 @@ const aj = arcjet({
       mode: "LIVE", // Blocks requests. Use "DRY_RUN" to log only
       // Block all bots except the following
       allow: [
-        "CATEGORY:SEARCH_ENGINE", // Google, Bing, etc
-        // Uncomment to allow these other common bot categories
-        // See the full list at https://arcjet.com/bot-list
-        //"CATEGORY:MONITOR", // Uptime monitoring services
-        //"CATEGORY:PREVIEW", // Link previews e.g. Slack, Discord
+        "CATEGORY:SEARCH_ENGINE",
       ],
     }),
     // Create a token bucket rate limit. Other algorithms are supported.
--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,14 +2,14 @@ import mongoose from "mongoose";
 import { ENV } from "./env.js";
 
 export const connectDB = async () => {
-    try {
-        await mongoose.connect(ENV.MONGO_URI, {
-            maxPoolSize: 10,
-            serverSelectionTimeoutMS: 5000
-        });
-        console.log('✅ MongoDB connected successfully');
-    } catch (error) {
-        console.error('❌ MongoDB connection error:', error.message);
-        process.exit(1);
-    }
+  try {
+    const { MONGO_URI } = ENV;
+    if (!MONGO_URI) throw new Error("MONGO_URI is not set");
+
+    const conn = await mongoose.connect(ENV.MONGO_URI);
+    console.log("MONGODB CONNECTED:", conn.connection.host);
+  } catch (error) {
+    console.error("Error connection to MONGODB:", error);
+    process.exit(1); // 1 status code means fail, 0 means success
+  }
 };
