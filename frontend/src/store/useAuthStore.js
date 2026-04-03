0d5eed10f49f4623250b89894784682da08c9a27
devraj42817 <rajdev20774@gmail.com>
2026-04-03 20:15:06 +0530

    fix: update BASE_URL for production socket connection

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:8000" : "/";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:8000" : "https://sololink-0jy9.onrender.com/socket.io/";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
