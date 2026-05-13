7cd26891e09e4d34e6ebd425fd914ba4c917ac80
devraj42817 <rajdev20774@gmail.com>
2026-05-13 14:39:24 +0530

    feat: initialize express server with middleware, routing, and database connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -32,13 +32,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoute);
 
-if (ENV.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (_, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-  });
-}
 
 // START SERVER
 server.listen(PORT, () => {
